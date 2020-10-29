a85c0f300b525aea3f2028c7eb8ccd876d8678ee
hongzhonglu <luho@chalmers.se>
2020-10-29 09:31:20 +0100

    feat: add new conservation of 15461 OGs

--- 6_other_small_tasks/protein_conservation_for_ML.R
+++ 6_other_small_tasks/protein_conservation_for_ML.R
@@ -53,5 +53,21 @@ write.table(conservation_score_filter, "data/conservation_score_complex_filter.t
 #write.table(ortholog_filter_for_complex, "data/conservation_score_complex_filter.txt")
 
 
+# for all the OG with species_num > 6
+# in these OGs, paralog exist
+conservation_score <- read_csv("data/conservation_score_sce_based_on_original_protein_align_15461.csv")
 
+conservation_score <- filter(conservation_score, !is.na(conservation_score))
+conservation_score$OG <- str_replace_all(conservation_score$OG, "_aa_aligned.fasta", "")
+ortholog <- read_tsv("data/ortholog_occurence_num_all.tsv")
+conservation_score$species_num <- getSingleReactionFormula(ortholog$species_num, ortholog$ID, conservation_score$OG)
+conservation_score$species_num <- as.numeric(conservation_score$species_num)
+conservation_score_filter <- conservation_score[conservation_score$species_num > 2, ]
 
+ggplot(conservation_score_filter, aes(conservation_score)) +
+  geom_histogram(aes(y = ..density..), alpha = 0.5, fill = "blue", bins = 50) +
+  xlim(0, 1) +
+  theme(panel.background = element_rect(fill = "white", colour = "black")) +
+  geom_density(colour = "black", alpha = 0.3, fill = "grey50") +
+  theme(axis.text = element_text(size = 16), axis.title = element_text(size = 20, face = "bold")) +
+  xlab("average_conservation_score")
